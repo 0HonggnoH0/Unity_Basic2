ab662bf6d06cb7b7def5714bf9509329bb69f21d
0HonggnoH0 <ghdtjqls0@naver.com>
2025-06-11 17:16:43 +0900

    Add_Renpy
    SData Use

--- Unity_Basic2/Assets/01_Script/Data/SData.cs
+++ Unity_Basic2/Assets/01_Script/Data/SData.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+using UnityEngine;
+
 public class SData : SingletonBehabiour<SData>
 {
     [SerializeField] Test testDatas; // DataTableÀÇ µ¥ÀÌÅÍ ÅÇ Name
@@ -9,6 +11,34 @@ public class SData : SingletonBehabiour<SData>
         for (int i = 0; i < TestData.Length; i++)
             if (TestData[i].ID_I == _id)
                 return TestData[i];
+        return null;
+    }
+
+    [SerializeField] Dialogue dialogueDatas; // DataTableÀÇ µ¥ÀÌÅÍ ÅÇ Name
+    public static DialogueData[] DialogueData { get { return Instance.dialogueDatas.dataArray; } } // µ¥ÀÌÅÍ ÅÇ Name + Data
+    public static DialogueData GetDialogueData(int _id)
+    {
+        for (int i = 0; i < DialogueData.Length; i++)
+        {
+            if (DialogueData[i].ID == _id)
+            {
+                return DialogueData[i];
+            }
+        }
+        return null;
+    }
+
+    [SerializeField] Character characterDatas; // DataTableÀÇ µ¥ÀÌÅÍ ÅÇ Name
+    public static CharacterData[] CharacterData { get { return Instance.characterDatas.dataArray; } } // µ¥ÀÌÅÍ ÅÇ Name + Data
+    public static CharacterData GetCharacterData(int _id)
+    {
+        for (int i = 0; i < CharacterData.Length; i++)
+        {
+            if (CharacterData[i].ID == _id)
+            {
+                return CharacterData[i];
+            }
+        }
         return null;
     }
 
